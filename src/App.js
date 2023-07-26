54fdd120c2600104ee0a0a724f1eb9eebffc3c08
stef-timmermans <timmerms@oregonstate.edu>
2023-07-25 23:03:15 -0700

    compiles, but coords load incorrectly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import DestinationButton from './DestinationButton';
+import locations from './data/locations.json';
 import mapImage from './images/jhoto-kanto-map.png';
 
 function App() {
+  const [selectedLocation, setSelectedLocation] = useState(null);
+  const [hoveredLocation, setHoveredLocation] = useState(null);
+
+  const handleLocationSelect = (id) => {
+    setSelectedLocation(locations.find(location => location.id === id));
+  };
+
+  const handleLocationHover = (id) => {
+    setHoveredLocation(locations.find(location => location.id === id));
+  };
+
+  const handleLocationLeave = () => {
+    setHoveredLocation(null);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={mapImage} alt="Map of Jhoto and Kanto" />
+        <img src={mapImage} alt="Map of Jhoto and Kanto" style={{position: 'relative'}} />
+        {locations.map((location) => 
+          <DestinationButton 
+            key={location.id} 
+            location={location}
+            onHover={handleLocationHover}
+            onLeave={handleLocationLeave}
+            onSelect={handleLocationSelect}
+          />
+        )}
       </header>
     </div>
   );
