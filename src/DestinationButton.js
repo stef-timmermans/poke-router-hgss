0f98510ea848af8e50b9fde6694efda434c57f3f
stef-timmermans <timmerms@oregonstate.edu>
2023-08-01 21:43:29 -0700

    fancier tooltip, styling to be added later :D

--- src/DestinationButton.js
+++ src/DestinationButton.js
@@ -1,23 +1,32 @@
 /*
-  Component: LocationButton
+  Component: DestinationButton
   Description:
-    The LocationButton component stores the
-    button required to navigate to a location.
-    Some locations require multiple buttons to
-    be defined as a more complex shape (for example,
-    and L-shaped location requires two buttons to
-    be defined). The LocationButton component
-    reads in from the locations data file to
-    determine the location's shape and coordinates
-    and renders the button accordingly.
+    A button that is placed on the map to represent a destination.
+    When the button is hovered over, a tooltip appears with the name
+    of the destination. When the button is clicked, the user is
+    navigated to the location page for that destination. Some locations
+    have multiple buttons to allow for more complex shapes than simply
+    rectangles.
+
+    Coordinate inputs are sourced from data/locations.json and are inclusive
+    lower bounds and inclusive upper bounds. For example, a button with the
+    following coordinates: [[0,0],[10,10]] would be an 11x11 pixel square with
+    its top left corner at (0,0) and its bottom right corner at (10,10).
 */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const DestinationButton = ({ location, onHover, onLeave }) => {
   const navigate = useNavigate();
   const urlDashedLocationName = location.name.replace(/\s+/g, '-'); // Replace spaces with hyphens
+  const [isHovering, setIsHovering] = useState(false);
+  const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event) => {
+    const { clientX: x, clientY: y } = event;
+    setTooltipPosition({ x: x + 20, y });
+  }
 
   return (
     <>
@@ -35,18 +44,44 @@ const DestinationButton = ({ location, onHover, onLeave }) => {
             position: 'absolute',
             top: `${shape[0][0]}px`,
             left: `${shape[0][1]}px`,
-            width: `${shape[1][0] - shape[0][0]}px`,
-            height: `${shape[1][1] - shape[0][1]}px`,
+            width: `${shape[1][0] - shape[0][0] + 1}px`,
+            height: `${shape[1][1] - shape[0][1] + 1}px`,
+
             backgroundColor: 'transparent',
             border: 'none',
             cursor: 'pointer',
+            borderRadius: '5px',
           }}
-          onMouseEnter={() => onHover(location.id)}
-          onMouseLeave={onLeave}
+          onMouseEnter={(event) => {
+            setIsHovering(true);
+            onHover(location.id);
+          }}
+          onMouseMove={handleMouseMove}
+          onMouseLeave={() => {
+            setIsHovering(false);
+            onLeave();
+          }}
           onClick={() => navigate(`/location/${urlDashedLocationName}`)}
-          title={location.name} // Add the location name as the button title
         />
       )}
+
+      <div
+        style={{
+          visibility: isHovering ? 'visible' : 'hidden',
+          position: 'fixed',
+          top: `${tooltipPosition.y}px`,
+          left: `${tooltipPosition.x}px`,
+          padding: '5px',
+          backgroundColor: 'white',
+          color: 'black',
+          borderRadius: '5px',
+          opacity: isHovering ? 1 : 0,
+          transition: 'opacity 0.2s ease-in-out, visibility 0.2s ease-in-out',
+          pointerEvents: 'none',
+        }}
+      >
+        {location.name}
+      </div>
     </>
   );
 };
